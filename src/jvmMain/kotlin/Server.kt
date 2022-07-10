448e737917af963e919fc673e89039eb0ec1144a
Aditya Pydimarri <aditya.pydimarri@gmail.com>
2022-07-10 12:35:50 -0500

    adding files

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,33 @@
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.litote.kmongo.*
+import org.litote.kmongo.coroutine.*
+import org.litote.kmongo.reactivestreams.KMongo
+import com.mongodb.ConnectionString
+
 fun main() {
-    println("Hello, JVM!")
+    embeddedServer(Netty, 9090) {
+        routing {
+            get("/hello") {
+                call.respondText("Hello, Stock!")
+            }
+            get("/") {
+                call.respondText(
+                    this::class.java.classLoader.getResource("index.html")!!.readText(),
+                    ContentType.Text.Html
+                )
+            }
+            static("/") {
+                resources("")
+            }
+        }
+    }.start(wait = true)
 }
